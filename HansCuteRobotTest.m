d2bf3a176085d4718c2c9bd64fdf423ed60dcbe4
Adam Scicluna <Adam.Scicluna@student.uts.edu.au>
2022-05-02 01:35:52 +1000

    Testing Hans Cute

--- HansCuteRobotTest.m
+++ HansCuteRobotTest.m
@@ -1,12 +1,17 @@
 clear all
-r = HansCute("cheese");
+r = HansCute;
 %r.plotModel
 %r.plotModel
 % workspace = [-4 4 -4 4 -4 4];                                       % Set the size of the workspace when drawing the robot
 % 
 % scale = 0.5;
 % 
-% q = zeros(1,7);                                                     % Create a vector of initial joint angles
+q = zeros(1,7);                                                     % Create a vector of initial joint angles
 
 %r.plot()               % Plot the robot
-r.teach();
+% disp('Press enter to continue:');
+% pause;
+% q2 = [pi/2, pi/2, 0, 0, 0, 0, 0];
+% r.robotModel.animate(q2);
+%r.robotModel.teach();
+r.plotModel();
